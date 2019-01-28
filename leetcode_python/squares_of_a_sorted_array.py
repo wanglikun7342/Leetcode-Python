d15d1199f816d28329e1818a9c515ad1c58ffcd4
eric <wanglikun7342@gmail.com>
2019-01-28 16:12:47 +0800

    change[]: Squares of a Sorted Array

---
+++ leetcode_python/squares_of_a_sorted_array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortedSquares(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(A)):
+            A[i] = pow(A[i], 2)
+        A = sorted(A)
+        return A
+
+
+if __name__ == '__main__':
+    array = [-4, -1, 0, 3, 10]
+    solution = Solution()
+    print(solution.sortedSquares(array))
